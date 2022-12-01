13705044945390255dd0cf53bad2262632461b1b
Nora <norapavlovabnb14@gmail.com>
2022-12-01 12:02:59 +0200

    Initial Commit

---
+++ meetups/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Participant
+
+
+class RegistrationForm(forms.Form):
+    email = forms.EmailField(label='Your email')
+    # class Meta:
+    #     model = Participant
+    #     fields = ['email']
